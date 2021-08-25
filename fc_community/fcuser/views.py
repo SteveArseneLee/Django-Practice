1d99a321050ae524e5bd0cdcad27c3abf0653742
SteveArseneLee <lclgood@naver.com>
2021-08-25 20:25:33 +0900

    210825

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -2,29 +2,49 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password, check_password
 from .models import Fcuser
+from .forms import LoginForm
+
+def home(request):
+    user_id = request.session.get('user')
+    
+    if user_id:
+        fcuser = Fcuser.objects.get(pk=user_id)
+        return HttpResponse(fcuser.username)
+        
+    return HttpResponse('Home!')
+
+def logout(request):
+    if request.session.get('user'):
+        del(request.session['user'])
+    
+    return redirect('/')
 
 def login(request):
-    if request.method == 'GET':
-        return render(request, 'login.html')
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+    form = LoginForm()
+    return render(request, 'login.html', {'form':form})
+    
+    # if request.method == 'GET':
+    #     return render(request, 'login.html')
+    # elif request.method == 'POST':
+    #     username = request.POST.get('username', None)
+    #     password = request.POST.get('password', None)
         
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야합니다.'
-        else:
-            # 다시 Fcuser라는 모델에서 정보를 가져옴
-            fcuser = Fcuser.objects.get(username=username)
-            if check_password(password, fcuser.password):
-                # 비밀번호가 일치, 로그인 처리
-                # 세션!
-                # redirect로 다시 홈으로 돌아가기
-                pass
-            else:
-                res_data['error'] = '비밀번호를 틀렸습니다.'
+    #     res_data = {}
+    #     if not (username and password):
+    #         res_data['error'] = '모든 값을 입력해야합니다.'
+    #     else:
+    #         # 다시 Fcuser라는 모델에서 정보를 가져옴
+    #         fcuser = Fcuser.objects.get(username=username)
+    #         if check_password(password, fcuser.password):
+    #             # 비밀번호가 일치, 로그인 처리
+    #             # 세션!
+    #             request.session['user'] = fcuser.id
+    #             # redirect로 다시 홈으로 돌아가기
+    #             return redirect('/') # /을 쓰면 현재 쓰는 웹 사이트의 root로 감
+    #         else:
+    #             res_data['error'] = '비밀번호를 틀렸습니다.'
         
-        return render(request, 'login.html', res_data)
+        # return render(request, 'login.html', res_data)
         
 
 # Create your views here.
